444e74bee75d9aabd13cbaf5433ab0af721483b9
EC2 Default User <ec2-user@ip-172-31-47-121.ap-northeast-1.compute.internal>
2022-12-06 12:22:20 +0000

    [Update]いいね機能追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,21 @@
 class FavoritesController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    # 非同期処理する際は、インスタンス変数に置き換える
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
-    redirect_to request.referer
-    # redirect_to book_path(book)
+    # 非同期処理:リダイレクトをコメントアウトするとリダイレクト先がない、かつJavaScriptリクエストという状況になる
+    # createアクション実行後は、create.js.erbファイルを探す
+    # redirect_to request.referer
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    # 非同期処理する際は、インスタンス変数に置き換える
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect_to request.referer
-    # redirect_to book_path(book)
+    # 非同期処理:リダイレクトをコメントアウトするとリダイレクト先がない、かつJavaScriptリクエストという状況になる
+    # destroyアクション実行後は、destroy.js.erbファイルを探す
+    # redirect_to request.referer
   end
 end
