67053bb2783ecc5e5c991fc1395a712ed1991201
EC2 Default User <ec2-user@ip-172-31-47-121.ap-northeast-1.compute.internal>
2022-12-07 07:48:02 +0000

    [Finish]コメント機能非同期化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,21 +1,29 @@
 class BookCommentsController < ApplicationController
 
+   #止めたいときは下のを使う
+    #binding.pry
+
   def create
-    book = Book.find(params[:book_id])
-    book_comment = current_user.book_comments.new(book_comment_params)
+    # 非同期化の為、インスタンス変数に変更
+    @book = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments.new(book_comment_params)
     # ↑上記のコードは省略記述
-    book_comment.book_id = book.id
-    book_comment.save
+    @book_comment.book_id = @book.id
+    @book_comment.save
+    # books_controllerのshowと同じ値を渡す
+    @book_comment = BookComment.new
     # １つ前の画面に
-    redirect_to request.referer
-    # redirect_to book_path(book)
+    # redirect_to request.referer
   end
 
   def destroy
     BookComment.find(params[:id]).destroy
+    # renderしたときに@bookのデータが無い為@bookを定義
+    @book = Book.find(params[:book_id])
+    # 投稿が残らないようにするために下を記述
+    @book_comment = BookComment.new
     # １つ前の画面に戻る
-    redirect_to request.referer
-    # redirect_to book_path(params[:book_id])
+    # redirect_to request.referer
   end
 
   private
