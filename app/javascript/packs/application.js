4ea73d1c9ec67afd980f0843e8cee3eb7cd91fa7
EC2 Default User <ec2-user@ip-172-31-47-121.ap-northeast-1.compute.internal>
2022-12-25 10:02:43 +0000

    [Start]レビュー機能

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,14 @@ import "bootstrap"
 import '@fortawesome/fontawesome-free/js/all';
 import "../stylesheets/application"
 
+// 5段階評価の実装
+import Raty from "raty.js"
+window.raty = function(elem,opt) {
+  let raty =  new Raty(elem,opt)
+  raty.init();
+  return raty;
+}
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
